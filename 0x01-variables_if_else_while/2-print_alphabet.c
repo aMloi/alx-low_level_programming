84e62852a07c6778e6f4ed20f176731b4a4f6f37
ali,moh <alimoubark5@gmail.com>
2022-12-26 15:27:44 -0800

    hs

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints all alphabet
+ * Return: 0
+ */
+int main(void)
+{
+ char c;
+
+      c = 'a';
+      while(c <= 'z')
+      {
+              printf"(%c\n", c);
+              c = c+1;
+      }
+      return (0);
+}
+
+
+
+
+
+
+
+
+}
