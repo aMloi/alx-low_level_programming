68b5b1c0c8389c3d8d83dc98c784aa5df883ba3e
ali,moh <alimoubark5@gmail.com>
2022-12-28 13:49:58 -0800

    ahhs

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * _isalpha - kdjsdkjfhscj
- *
- * Return: 0 or 1
- */
+* _isalpha - kdjsdkjfhscj
+*@c: jedhjehd
+*
+* Return: 0 or 1
+*/
 int _isalpha(int c)
 {
 
-if ((c >= 65 && c <= 90) || (c >97 && c < 122))
+if ((c >= 65 && c <= 90) || (c > 97 && c < 122))
 {
 	return (1);
 }
