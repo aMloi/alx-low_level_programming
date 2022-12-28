db7bc32db91eb89cae012627d737c8baabf3b86d
ali,moh <alimoubark5@gmail.com>
2022-12-28 13:41:45 -0800

    ahhs

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
  * _islower - dkcngchxj
+ * @c: djcdjschcj
+ *
  * Return: 0 or 1
  */
 int _islower(int c)
 {
 
-if (c >= 97 && c<= 122)
+if (c >= 97 && c <= 122)
 {
-	return(1);
+	return (1);
 }
 return (0);
 
