b42e9702a096619bae985c14ded134b2682e6441
ali,moh <alimoubark5@gmail.com>
2022-12-28 14:03:09 -0800

    hha

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *print_sign
+ *@n: shsdjcd
+ *Return: 0 or 1
+ */
+include "main.h"
+
+int print_sign(int n)
+{	
+{
+if (n > 0)
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+putchar(48);
+return (0);
+}
+}
