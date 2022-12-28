ca57de6c900b5d3c3ef6d20493c41920b5f2cc5e
ali,moh <alimoubark5@gmail.com>
2022-12-28 13:22:55 -0800

    hs

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - mscadhcdc
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+int i;
+int j;
+
+for (i = 0; i<= 10; i++)
+{
+char ch = 'a';
+for(j = ch; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+
+_putchar('\n');
+
+};
+
+
+}
