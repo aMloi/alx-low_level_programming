670ec643f353f337f1ed672528d78e2c2ae2455f
ali,moh <alimoubark5@gmail.com>
2023-01-02 12:59:11 -0800

    shds

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
- * main - jschhdcj
- * @i: dfjdofjvdbrgdf
- *
- * Return: 0
- */
+* main - Test function for positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 int i;
@@ -13,6 +13,5 @@ i = 0;
 positive_or_negative(i);
 
 return (0);
-
-
 }
+
