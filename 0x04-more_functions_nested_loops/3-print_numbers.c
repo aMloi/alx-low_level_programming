333460104bfb4700b01c0eee68d2f6d26a8dde00
ali,moh <alimoubark5@gmail.com>
2023-01-02 15:29:28 -0800

    dhsd'

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+* print_numbers - checks for uppercase character
+* Return: Always 0.
+*/
+void print_numbers(void)
+{
+
+int i;
+for (i = 0; i < 10; i++)
+{
+
+_putchar(i);
+
+}
+_putchar("\n");
+
+}
