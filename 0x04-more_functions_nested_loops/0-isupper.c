579d0acbbe6257a5e92c33e5da9d39f196fb8701
ali,moh <alimoubark5@gmail.com>
2023-01-02 14:42:52 -0800

    dhs

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _isupper(int c) - sofljvijvd
+ * @c: sdhvidjvdfvo
+ * Return: 1 or 0
+ */
+int _isupper(int c)
+{
+
+if (c <= 90 && c >= 65)
+{	return (1);}
+else
+return (0);
+
+
+
+}
