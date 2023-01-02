7cc23fa13a244026dc24eb332adc86b73017e5b5
ali,moh <alimoubark5@gmail.com>
2023-01-02 14:44:35 -0800

    dhs

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,15 @@
 /*
  * _isupper(int c) - sofljvijvd
  * @c: sdhvidjvdfvo
+ *
  * Return: 1 or 0
  */
 int _isupper(int c)
 {
 
 if (c <= 90 && c >= 65)
-{	return (1);}
+{	return (1);
+}
 else
 return (0);
 
