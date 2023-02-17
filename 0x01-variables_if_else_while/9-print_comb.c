b08de2268b23acf02e77de5da20495ee949fa5ea
alii <alimoubark5@gmail.com>
2023-02-17 15:56:52 -0800

    shsds

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+/**
+ * main - dgcfsddc
+ * Return: jsdfhurgfs
+ */
+int main(void)
+{
+
+int a = 0;
+
+for (a = 0; a <= 9; a++)
+{
+
+putchar(a + '0');
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+
+
+}
+
+
+
+
+
+}
