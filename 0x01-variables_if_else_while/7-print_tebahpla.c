59991917e9bd1626fabd7d4c173fd8780d15f4b8
alii <alimoubark5@gmail.com>
2023-02-17 15:38:43 -0800

    djsf' git push

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*main - djhcjgfv
+ * Return: dhfsgf
+ *
+ */
+int main(void)
+{
+	int a = 122;
+	for (a = 122; a >= 97; a--)
+	{	putchar(a);
+	}
+	putchar('\n');
+
+return (0);
+
+
+
+
+
+}
