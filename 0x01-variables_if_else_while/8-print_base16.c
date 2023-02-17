2af28a188ae51af8e5894b8115ed16e2d0eae90e
alii <alimoubark5@gmail.com>
2023-02-17 15:49:14 -0800

    djsf' git push

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - djgfgsfr
+ * Return: kdhiciusfc
+ *
+ */
+int main(void)
+{
+
+char a = 97;
+int b = 0;
+
+for (b = 0; b <= 9; b++)
+{
+putchar(b + '0');
+
+}
+for (a = 97; a <= 102; a++)
+{
+putchar(a);
+}
+putchar('\n');
+
+return (0);
+
+}
