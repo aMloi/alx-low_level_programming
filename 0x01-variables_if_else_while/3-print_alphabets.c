f447dd5ddc63f26e851bc14f79b9e662358cbb25
alii <alimoubark5@gmail.com>
2023-02-17 14:37:35 -0800

    djsf' git push

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - djhgfgsf
+ * Return: 0
+ *
+ */
+int main(void)
+{
+
+char a, b;
+a = 97;
+b = 65;
+
+while (a <= 122)
+{
+putchar (a);
+a++;
+}
+while (b <= 90)
+{
+putchar (b);
+b++;
+}
+putchar ('/n');
+
+return (0);
+}
