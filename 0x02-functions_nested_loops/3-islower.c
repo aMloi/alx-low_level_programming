cd291687ab3f536ab57c9393eac3ed78ab307f6f
alii <alimoubark5@gmail.com>
2023-02-21 11:38:43 -0800

    aejhfs

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _islower - xhfvgjsrf
+*@c: ifhgdhfd
+*
+* Return: jdfgvydrg
+*/
+int _islower(int c)
+{
+
+
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+
+}
+else
+return (0);
+
+
+
+}
