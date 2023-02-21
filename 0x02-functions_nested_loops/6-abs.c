46b2467e83a0ff0cecaccca3d5ed56ccf2fec543
alii <alimoubark5@gmail.com>
2023-02-21 12:30:49 -0800

    sdhfs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _abs - ajgefsgfs
+ * @n: skdhfhf
+ *
+ * Return: xjdhvkgv
+ */
+int _abs(int n)
+{
+
+if (n < 0)
+{
+n = n * -1;
+
+}
+else
+{
+n = n;
+}
+
+return (n);
+
+
+
+
+}
