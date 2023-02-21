f40758f6a973d6420ccd8904038435018996ce13
alii <alimoubark5@gmail.com>
2023-02-21 11:43:15 -0800

    aejhfs

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef MYLIB_H
-#define MYLIB_H
+#ifndef main_h
+#define main_h
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,4 +14,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 
-#endif /* MYLIB_H */
+#endif /* main_h */
