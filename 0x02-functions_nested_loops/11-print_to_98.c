4f1821015dda1a828c577405aac16bb91e49d03f
alii <alimoubark5@gmail.com>
2023-02-21 13:28:35 -0800

    sdhfs

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - sjdhfshff
+ * @n: jdfhsrfh
+ *
+ * Return: sjdhfh
+ */
+void print_to_98(int n)
+{
+
+if (n <= 98)
+	{
+for (n = n; n < 99; n++)
+{
+
+printf("%d, ", n);
+
+
+}
+}
+else
+{
+
+for (n = n; n < 99; n--)
+	printf("%d, ", n);
+
+
+
+}
+
+
+
+}
