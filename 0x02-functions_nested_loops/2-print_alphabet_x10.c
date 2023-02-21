da30b0566f76c27c3f77f2a91abbe1a2dd9041e7
alii <alimoubark5@gmail.com>
2023-02-21 11:17:24 -0800

    djshfs

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - hsgrhliskhgdl
+ *
+ *
+ * Return: dfhsuhfs
+ */
+void print_alphabet_x10(void)
+{
+	int b = 0;
+while (b < 9)
+{
+	int a = 'a';
+		while (a <= 'z')
+		{
+			_putchar(a);
+a++;
+
+
+		}
+		_putchar('\n');
+
+}
+}
