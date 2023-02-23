0c2c61d8973ce69e7c7a4697c846a8ef375ee6c6
alii <alimoubark5@gmail.com>
2023-02-23 08:03:38 -0800

    dskhdfhf

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * mul - print multioaladcjsdc jszhcjsh
+ * @a: ajdhcks sdjhcc
+ * @b: dhcjsidhc
+ *
+ * Return: int
+ */
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+
+
+
+
+}
