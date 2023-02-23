6c295a6441bed6f486e7c230f9d6d3d6a17faedb
alii <alimoubark5@gmail.com>
2023-02-23 09:03:39 -0800

    sjdhsjfhs

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - xjhvxhv zkjdbvxbcb jxv
+ * @n: xkhfvdfhv jvdfhv
+ *
+ * Return: dfshc djhfhc
+ */
+void print_line(int n)
+{
+
+int i;
+for (i = 0; i <= n; i++)
+{
+
+	if (n == 0)
+	{
+		break;
+	}
+
+	_putchar('_');
+}
+
+	_putchar('\n');
+}
