23b1acc35df5d92741d3e8378f21ebb79aa2bd3e
alii <alimoubark5@gmail.com>
2023-02-23 07:39:21 -0800

    sjdhfsf

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *
+ * -isupper - xdhkfghfkugf
+ *  @c: jxdhfxgv
+ *
+ *  Return: ksdhfhd
+ */
+int _isupper(int c)
+{
+
+if (c >= 'A' && c <= 'Z')
+	return (1);
+else
+	return (0);
+
+
+}
