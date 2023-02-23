a64a5373bddb4f7e56960aa1ba10bd09c9306b7b
alii <alimoubark5@gmail.com>
2023-02-23 09:39:44 -0800

    sjdhsjfhs

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - jkdhgfusrfhf ihfvdfhv
+ * @size: jsdhfhfdf
+ *
+ * Return: zjdhc kncf
+ */
+void print_square(int size)
+{
+int i, j;
+for (i = 0; i < size; i++)
+{
+
+for (j = 1; j < size; j++)
+{
+putchar('#');
+
+
+}
+
+
+putchar('#');
+
+putchar('\n');
+
+}
+
+}
