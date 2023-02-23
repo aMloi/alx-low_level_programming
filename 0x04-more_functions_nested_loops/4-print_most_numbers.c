bc3106779a0683a37108ab8989e512452285b09b
alii <alimoubark5@gmail.com>
2023-02-23 08:21:33 -0800

    dskhdfhf

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_most_numbers - skehfhf jzhfskhfhr
+ *
+ * Return: void nothing
+ */
+void print_most_numbers(void)
+{
+
+int i;
+for (i = 48; i <= 57; i++)
+{
+
+if (i == 50 || i == 52)
+{
+
+continue;
+}
+else
+putchar(i);
+
+}
+putchar('\n');
+}
+
+
+}
