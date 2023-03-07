63bbb74a693b74de9f33f36a14841437bd92f812
alii <alimoubark5@gmail.com>
2023-03-06 17:07:38 -0800

    ajeuw

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main_h"
+/**
+ * swap_int - whejehe ahwhahha
+ * @a: whwhhwhyw ahaha
+ * @b: wahhahaah ahaha
+ *
+ * Return: whahaha
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
