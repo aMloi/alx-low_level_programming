1446877051a247cac6071a491f6b2b9946ee81fa
alii <alimoubark5@gmail.com>
2023-03-07 07:44:11 -0800

    ajeuw

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - whqqywy wuwywyw
+ * @str: wahwhahwh shwyah
+ *
+ * Return: whahah ahwhw
+ */
+void _puts(char *str)
+{
+	int i;
+
+	while (str[i] != '/0')
+	{ _puts(str[i]);
+		str[i++];
+	}
+	_puts('\n');
+}
